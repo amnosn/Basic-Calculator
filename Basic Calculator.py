7b38f9b4314c90d8d746a2fa0d1355ae45df96e0
Aman Osan <amanosan121@gmail.com>
2020-10-03 02:31:51 +0530

    added more functionalities

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -10,8 +10,11 @@ def subtract(num1, num2):
 def multiply(num1, num2):
     return num1 * num2
 
+def remainder(num1, num2):
+    return num1 % num2
+
 print("Please select one of the following options:\n")
-print("1.Add\n2.Subtract\n3.Divide\n4.Multiply")
+print("1.Add\n2.Subtract\n3.Divide\n4.Multiply\n5.Remainder\n")
 choice = int(input())
 num1 = float(input("Enter number 1:"))
 num2 = float(input("Enter number 2:"))
@@ -24,5 +27,7 @@ elif choice == 3:
     print(divide(num1, num2))
 elif choice == 4:
     print(multiply(num1, num2))
+elif choice == 5:
+    print(remainder(num1, num2))
 else:
     print("Not a valid option")
